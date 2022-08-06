4c26ec6e465e411138f7d9ff2bb8ee0ede56ce00
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-08-06 19:19:54 -0300

    n sei como mas deu certo

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,34 @@ app.get('/', (req, res) => {
     res.send('Hello World !');
 })
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+    console.log(`
+
+    #Projeto base https://github.com/augustoaraujoo/delivery-api
+
+    [npm init -y]
+    [git init]
+    [Procfile]
+    
+    *PRISMA*
+    [url      = env("DATABASE_URL")
+    shadowDatabaseUrl = env("SHADOW_DATABASE_URL")]
+    * /PRISMA*
+
+    yarn add dotenv
+    tsconfig.json é padrão no prisma.io
+
+
+    set DATABASE_URL=postgres://postgres:postgres@localhost:5432/postgres
+
+    set shadowDatabaseUrl=postgres://postgres:postgres@localhost:5432/postgres_shadow
+
+
+
+    [create heroku] = cli
+    [create db]
+    se der pau no git ![remote rejected] master->master
+        n sei como mas deu certo a build, 
+        só remover o yarn-error.log
+        yarn.lock 
+    ${PORT}`);
 })
