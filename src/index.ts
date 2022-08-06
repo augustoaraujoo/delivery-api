eaa5c92041fefcf66c9cb1702a111272b210d68f
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-08-06 18:28:29 -0300

    teste build

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+const PORT = process.env.PORT || 6060;
+
+const app = express();
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
