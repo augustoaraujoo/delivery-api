866a93759786107662ff2e748f7b77a191b5a988
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-08-06 18:44:00 -0300

    make it better

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 6060;
 const app = express();
 app.use(express.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World !');
 })
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
