84655bfbd2aedeb4540473a5bad34776a71da503
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-08-09 16:24:33 -0300

    --fix retorno somente das entregas que não foram aceitas pelo entregador

--- src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
+++ src/modules/deliveries/useCases/findAllAvailable/FindAllAvailableUseCase.ts
@@ -5,7 +5,8 @@ class FindAllAvailableUseCase {
     async execute(): Promise<any> {
         const deliveries = await prisma.deliveries.findMany({
             where: {
-                end_date: null
+                end_date: null,
+                id_deliveryman: null
             }
         })
         return deliveries;
