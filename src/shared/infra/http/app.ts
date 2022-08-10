b3ce9893a27f47ad93da4fe7ac3245d725119ae9
augustoroocha <augustinho_darocha1311@hotmail.com>
2022-08-10 20:19:48 -0300

    resolve swaggerjson

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,13 @@
 require('dotenv').config();
 import express, { NextFunction, Request, Response } from 'express'
+import swaggerUI from 'swagger-ui-express'
+import swaggerFile from '../../../swagger.json';
 import 'express-async-errors';
 import { routes } from './routes';
 
 const app = express();
 app.use(express.json());
+app.use("/", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.use(routes)
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     if (err instanceof Error) {
