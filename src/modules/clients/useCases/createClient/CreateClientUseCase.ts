c3a32320920cd07c8d12d35f7dbd6acace0f88eb
Augusto Araujo da Rocha <81258318+augustoaraujoo@users.noreply.github.com>
2023-07-29 23:35:04 -0300

    Update CreateClientUseCase.ts
    obg clean arch

--- src/modules/clients/useCases/createClient/CreateClientUseCase.ts
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -5,6 +5,7 @@ class CreateClientUseCase {
     constructor(private clientRepository: ClientRepository) { }
 
     async execute({ username, password }: IRequest): Promise<any> {
+    // não será necessário trocar nada aqui caso haja uma TROCA DE ORM graças a interface que implementa e respeita o InterfaceRepository
         const clientExists = await this.clientRepository.findFirst(username);
 
         if (clientExists) {
